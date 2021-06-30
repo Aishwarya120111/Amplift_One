884003e7f34c8dbca3093dcfb28f638c7eb3219c
Aishwarya120111 <nadimpallyaishwarya@outlook.com>
2021-06-30 17:42:04 +0530

    Uploaded

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {Container, Button, Form} from 'react-bootstrap';
+import Amplify from "aws-amplify";
+import { API } from 'aws-amplify';
+
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+async function addContact() {
+  const data = {
+    body: {
+      FirstName: formState.FirstName,
+      LastName: formState.LastName,
+      MailId: formState.MailId,
+      FeedbackMessage: formState.FeedbackMessage
+    }
+  };
+
+  console.log(data);
+  const apiData = await API.post('feedbackmap', '/feedform', data);
+  console.log({ apiData });
+  alert('Mail sent');
+}
+
+const formState = { FirstName: '', LastName: '', MailId: '', FeedbackMessage: '' };
+
+function updateFormState(key, value) {
+  formState[key] = value;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+    <div>
+      <h3>Get in touch</h3>
+      <br/>
+        <Form>
+          <Form.Group>
+            <Form.Label>FirstName</Form.Label>
+            <Form.Control placeholder="FirstName" onChange={e => updateFormState('FirstName', e.target.value)} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>LastName</Form.Label>
+            <Form.Control placeholder="LastName" onChange={e => updateFormState('LastName', e.target.value)} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>MailId</Form.Label>
+            <Form.Control placeholder="MailId" onChange={e => updateFormState('MailId', e.target.value)} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>FeedbackMessage</Form.Label>
+            <Form.Control placeholder="FeedbackMessage" onChange={e => updateFormState('FeedbackMessage', e.target.value)} />
+          </Form.Group>
+          <Button onClick={addContact}>Send a message</Button>
+        </Form>
+      </div>
+    </Container>
   );
 }
 
