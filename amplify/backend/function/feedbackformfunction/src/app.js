9fc3ff62d29b1bd526686d504cd57f3a298c8d31
Aishwarya120111 <nadimpallyaishwarya@outlook.com>
2021-07-01 23:03:47 +0530

    Newly commit

--- amplify/backend/function/feedbackformfunction/src/app.js
+++ amplify/backend/function/feedbackformfunction/src/app.js
@@ -40,10 +40,10 @@ app.post('/feedform', function(req, res) {
   var params = {
     TableName : process.env.STORAGE_FEEDBACKFORM_NAME,
     Item : {
-      FirstName: req.body.FirstName,
-      LastName: req.body.LastName,
-      Mailid: req.body.Mailid,
-      FeedbackMessage: req.body.FeedbackMessage
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      mailid: req.body.mailid,
+      feedbackmessage: req.body.feedbackmessage
     }
   }
   docClient.put(params, function(err,data){
--- src/App.js
+++ src/App.js
@@ -10,20 +10,20 @@ Amplify.configure(awsExports);
 async function addContact() {
   const data = {
     body: {
-      FirstName: formState.FirstName,
-      LastName: formState.LastName,
-      MailId: formState.MailId,
-      FeedbackMessage: formState.FeedbackMessage
+      firstname: formState.firstname,
+      lastname: formState.lastname,
+      mailid: formState.mailid,
+      feedbackmessage: formState.feedbackmessage
     }
   };
 
   console.log(data);
   const apiData = await API.post('feedbackmap', '/feedform', data);
   console.log({ apiData });
-  alert('Mail sent');
+  alert('Feedback Sent Successfully');
 }
 
-const formState = { FirstName: '', LastName: '', MailId: '', FeedbackMessage: '' };
+const formState = { firstname: '', lastname: '', mailid: '', feedbackmessage: '' };
 
 function updateFormState(key, value) {
   formState[key] = value;
@@ -33,24 +33,24 @@ function App() {
   return (
     <Container>
     <div>
-      <h3>Get in touch</h3>
+      <h3>Feedback Form</h3>
       <br/>
         <Form>
           <Form.Group>
-            <Form.Label>FirstName</Form.Label>
-            <Form.Control placeholder="FirstName" onChange={e => updateFormState('FirstName', e.target.value)} />
+            <Form.Label>First Name</Form.Label>
+            <Form.Control placeholder="FirstName" onChange={e => updateFormState('firstname', e.target.value)} />
           </Form.Group>
           <Form.Group>
-            <Form.Label>LastName</Form.Label>
-            <Form.Control placeholder="LastName" onChange={e => updateFormState('LastName', e.target.value)} />
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control placeholder="LastName" onChange={e => updateFormState('lastname', e.target.value)} />
           </Form.Group>
           <Form.Group>
-            <Form.Label>MailId</Form.Label>
-            <Form.Control placeholder="MailId" onChange={e => updateFormState('MailId', e.target.value)} />
+            <Form.Label>Mail Id</Form.Label>
+            <Form.Control placeholder="MailId" onChange={e => updateFormState('mailid', e.target.value)} />
           </Form.Group>
           <Form.Group>
-            <Form.Label>FeedbackMessage</Form.Label>
-            <Form.Control placeholder="FeedbackMessage" onChange={e => updateFormState('FeedbackMessage', e.target.value)} />
+            <Form.Label>Feedback Message</Form.Label>
+            <Form.Control placeholder="FeedbackMessage" onChange={e => updateFormState('feedbackmessage', e.target.value)} />
           </Form.Group>
           <Button onClick={addContact}>Send a message</Button>
         </Form>
